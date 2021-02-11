83fea28297132ff41d7e3be25a9613d5be6655a7
Dmytro Kruhlyi <dimakruhly@gmail.com>
2021-02-11 14:46:26 +0200

    basic nodejs api without any libraries and frameworks

---
+++ nodeAPI/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
+const server = require('./route.js'); // imports the routing file
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
